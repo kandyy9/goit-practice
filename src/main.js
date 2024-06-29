01e67eeca2ca061b58b4a44ee9ac95d3c0d42de5
Myroslav Raks <raksmyroslav@gmail.com>
2024-06-29 10:25:10 +0200

    task 4

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
+import { nanoid } from 'nanoid';
+
 const taskForm = document.querySelector('#task-form');
 const taskList = document.querySelector('#task-list');
-const taskKey = "task_key";
-  
-  
+const taskKey = 'task_key';
+
 //TODO-1
 // Напишіть логіку обробнику подій по сабміту
 // При сабміті треба у змінну записувати значення поля інпута
@@ -12,16 +13,17 @@ taskForm.addEventListener('submit', evt => {
   evt.preventDefault();
   const value = evt.target.elements.taskName.value.trim();
   if (!value) return;
+  const id = nanoid();
   console.log(value);
-  addItem(value);
-  addLocalStorage(value);
+  addItem(value, id);
+  addLocalStorage(value, id);
   evt.target.reset();
 });
 
-function addItem(item) {
+function addItem(item, id) {
   taskList.insertAdjacentHTML(
     'beforeend',
-    `<li>${item} <button type="button">X</button></li>`
+    `<li id="${id}">${item} <button type="button">X</button></li>`
   );
 }
 
@@ -29,10 +31,11 @@ function addItem(item) {
 // Написати функцію, яка при сабміті буде зберігати данні в сховище по ключу task_key, в сховище повинні додаватись таски,
 // а не перезаписуватись існуюча
 
-
-function addLocalStorage(value) {
+function addLocalStorage(value, id) {
   const allValue = JSON.parse(localStorage.getItem(taskKey)) ?? [];
-  
-  allValue.push(value);
+  allValue.push({ id, text: value });
   localStorage.setItem(taskKey, JSON.stringify(allValue));
 }
+
+//TODO-4
+//Відформатуйте код таким чином, щоб данні в сховищі зберігались у вигляді об'єкта { id: value, text: value}, розмітка додавалась з айдішніком на елемент списку li, айдішнік генерувати з допомогою бібліотеки nanoid, її треба встановити

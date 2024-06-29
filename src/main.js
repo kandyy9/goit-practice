e448f2cb7a37055eb43ad733ec911b971c62b368
Taisiia <159428493+Taisiia-Mazur@users.noreply.github.com>
2024-06-29 11:02:51 +0300

    add task-3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const taskForm = document.querySelector('#task-form');
 const taskList = document.querySelector('#task-list');
-
+const taskKey = "task_key";
+  
+  
 //TODO-1
 // Напишіть логіку обробнику подій по сабміту
 // При сабміті треба у змінну записувати значення поля інпута
@@ -12,6 +14,7 @@ taskForm.addEventListener('submit', evt => {
   if (!value) return;
   console.log(value);
   addItem(value);
+  addLocalStorage(value);
   evt.target.reset();
 });
 
@@ -20,4 +23,16 @@ function addItem(item) {
     'beforeend',
     `<li>${item} <button type="button">X</button></li>`
   );
+}
+
+//TODO-3
+// Написати функцію, яка при сабміті буде зберігати данні в сховище по ключу task_key, в сховище повинні додаватись таски,
+// а не перезаписуватись існуюча
+
+
+function addLocalStorage(value) {
+  const allValue = JSON.parse(localStorage.getItem(taskKey)) ?? [];
+  
+  allValue.push(value);
+  localStorage.setItem(taskKey, JSON.stringify(allValue));
 }

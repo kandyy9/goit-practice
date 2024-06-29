563044103a089bf088a5f467cf8b3cf49ef7a325
Serhii Reshetniak <reshet@lozovaya.org.ua>
2024-06-29 10:31:08 +0300

    Add items

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
-const taskForm = document.querySelector("#task-form");
-const taskList = document.querySelector("#task-list");
+const taskForm = document.querySelector('#task-form');
+const taskList = document.querySelector('#task-list');
 
 //TODO-1
 // Напишіть логіку обробнику подій по сабміту
 // При сабміті треба у змінну записувати значення поля інпута
 // Повинна бути перевірка на порожнє поле.
 
-taskForm.addEventListener("submit", (evt) => {
-    evt.preventDefault();
-    const value = evt.target.elements.taskName.value.trim();
-    if (!value) return 
-    console.log(value);
-    evt.target.reset();
+taskForm.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const value = evt.target.elements.taskName.value.trim();
+  if (!value) return;
+  console.log(value);
+  addItem(value);
+  evt.target.reset();
 });
+
+function addItem(item) {
+  taskList.insertAdjacentHTML(
+    'beforeend',
+    `<li>${item} <button type="button">X</button></li>`
+  );
+}
